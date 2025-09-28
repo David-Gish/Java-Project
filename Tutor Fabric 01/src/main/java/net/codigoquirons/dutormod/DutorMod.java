8e71cfcbd4e44b5e3eb32705e853a0096acf5a67
Leyson David Asprilla Mosquera <145991327+Leyson-Gith@users.noreply.github.com>
2025-09-28 14:46:06 -0500

    Prueba Tutorial mod
    Introduciendo codigo sobre fabric del  video juego de minecraft

---
+++ Tutor Fabric 01/src/main/java/net/codigoquirons/dutormod/DutorMod.java
@@ -0,0 +1,16 @@
+package net.codigoquirons.dutormod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DutorMod implements ModInitializer {
+
+    public static final String MOD_ID = "dutormod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ Tutor Fabric 01/src/main/java/net/codigoquirons/dutormod/DutorModClient.java
@@ -0,0 +1,11 @@
+package net.codigoquirons.dutormod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DutorModClient implements ClientModInitializer {
+@Override
+    public void onInitializeClient() {
+
+    }
+
+}
